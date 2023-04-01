744bd9555db2f3b83a767304cac1fe06b9f12b31
Reed Martin <reedhmartin@gmail.com>
2023-04-01 10:15:37 -0400

    Added starter code with TODOs to stylesheet and js file.

---
+++ script.js
@@ -0,0 +1,27 @@
+const apiKey = "YOUR_API_KEY";
+let city = "";
+
+// TODO: Create function to get weather data from API
+function getWeatherData() {
+  
+}
+
+// TODO: Create function to display current weather data
+function displayCurrentWeather() {
+  
+}
+
+// TODO: Create function to display forecast weather data
+function displayForecast() {
+  
+}
+
+// TODO: Create function to handle form submission
+function handleFormSubmit(event) {
+  
+}
+
+// TODO: Add event listener to form submit button
+document
+  .getElementById("search-button")
+  .addEventListener("click", handleFormSubmit);

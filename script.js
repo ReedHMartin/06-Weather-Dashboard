d807cef991bdd26c28ea04ceaefef67d259ba8cc
Reed Martin <reedhmartin@gmail.com>
2023-04-01 11:55:15 -0400

    Added API key. Added function to get weathe data.

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const apiKey = "YOUR_API_KEY";
+const apiKey = "b06ae261d171615a8cdaf2c9b12daf3a";
 let city = "";
 
 // TODO: Create function to get weather data from API
-function getWeatherData() {
-  
+function getWeatherData(city) {
+  // Construct URL for API request
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+  // Fetch weather data from API
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      // Pass data to displayCurrentWeather function
+      displayCurrentWeather(data);
+    })
+    .catch(error => {
+      console.log("Error:", error);
+    });
 }
 
 // TODO: Create function to display current weather data

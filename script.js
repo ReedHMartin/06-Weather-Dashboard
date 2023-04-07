bce60406cfb7763b87e10e31a1504accb485c273
Reed Martin <reedhmartin@gmail.com>
2023-04-07 16:43:30 -0400

    Added function to display weather forcast.

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 const apiKey = "b06ae261d171615a8cdaf2c9b12daf3a";
 let city = "";
 
-// TODO : Create function to get weather data from API
-function getWeatherData(city) {
-  // Construct URL for API request
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
-  // Fetch weather data from API
+function getWeatherData(city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
   fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-      // Pass data to displayCurrentWeather function
       displayCurrentWeather(data);
     })
     .catch(error => {
@@ -18,7 +14,6 @@ function getWeatherData(city) {
     });
 }
 
-// TODO : Create function to display current weather data
 function displayCurrentWeather(data) {
   const cityNameEl = document.getElementById("city-name");
   const temperatureEl = document.getElementById("temperature");
@@ -32,22 +27,38 @@ function displayCurrentWeather(data) {
   windSpeedEl.textContent = `${Math.round(data.wind.speed)} mph`;
 }
 
-// TODO: Create function to display forecast weather data
-function displayForecast() {
-  const forecastEl = document.getElementById("forecast");
-  fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    const dailyData = data.list.filter(item => item.dt_txt.includes("12:00:00"));
+  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`;
 
-     let forecastHtml = "";
-     dailyData.forEach(day => {
-      const date = new Date(day.dt * 1000);
-      const icon = day.weather[0].icon;
-      const temp = `${Math.round(day.main.temp)}°F`;
-      const wind = `${Math.round(day.wind.speed)} mph`;
-      const humidity = `${day.main.humidity}%`;
-}
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const dailyData = data.list.filter(item => item.dt_txt.includes("12:00:00"));
+
+      let forecastHtml = "";
+      dailyData.forEach(day => {
+        const date = new Date(day.dt * 1000);
+        const icon = day.weather[0].icon;
+        const temp = `${Math.round(day.main.temp)}°F`;
+        const wind = `${Math.round(day.wind.speed)} mph`;
+        const humidity = `${day.main.humidity}%`;
+
+        forecastHtml += `
+          <div class="forecast-day">
+            <p class="forecast-date">${date.toLocaleDateString()}</p>
+            <img src="https://openweathermap.org/img/w/${icon}.png" alt="${day.weather[0].description}" />
+            <p class="forecast-temp">${temp}</p>
+            <p class="forecast-wind">${wind}</p>
+            <p class="forecast-humidity">${humidity}</p>
+          </div>
+        `;
+      });
+
+      forecastEl.innerHTML = forecastHtml;
+    })
+    .catch(error => {
+      console.log("Error:", error);
+    });
+
 
 // TODO: Create function to handle form submission
 function handleFormSubmit(event) {
